a3621efb2926ffbead5d7b0f16178c9978642232
GsrBhat <gsrbhat20@gmail.com>
2026-03-30 14:13:01 +0530

    Fix DataSource import to javax.sql

--- src/main/java/com/hospital/config/DatabaseConfig.java
+++ src/main/java/com/hospital/config/DatabaseConfig.java
@@ -1,6 +1,6 @@
 package com.hospital.config;
 
-import jakarta.sql.DataSource;
+import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
